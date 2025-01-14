37b7950e36359cbf3cafa723d52e2a3e431046d9
Eous-morning-star <135666750+Eous-morning-star@users.noreply.github.com>
2025-01-14 07:46:27 +0100

    Update indorama.py

--- indorama.py
+++ indorama.py
@@ -372,7 +372,7 @@ def calculate_kpis(file_path):
     }
 
 # Display the logo at the top of the homepage
-st.image(indorama_logo.png, use_container_width=True)
+st.image("indorama_logo.png", use_container_width=True)
 
 # Main Page Functionality
 if "page" not in st.session_state:
